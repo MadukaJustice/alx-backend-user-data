725352261fabb5f2474de615a5d22ad183799b25
MadukaJustice <jonasjustice98@gmail.com>
2023-08-08 17:58:22 +0000

    Update all

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
 from api.v1.views import app_views
+from flask import abort, jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -14,22 +14,22 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
-      - 401 unauthorized error
+      - abort(401)
     """
-    return abort(401)
+    abort(401)
 
 
-@app_views.route('/forbidden', strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden
     Return:
-      - 403 forbidden error
+      - abort(403)
     """
-    return abort(403)
+    abort(403)
 
 
 @app_views.route('/stats/', strict_slashes=False)
